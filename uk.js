1864733c7dcae1d103bfd205a928f966efa84a67
Sergey Yurkiv <sergiy.24ok@gmail.com>
2016-08-17 19:31:50 +0300

    add redirect

---
+++ uk.js
@@ -0,0 +1,38 @@
+var redirect = (function () {
+    /* This implements the actual redirection. */
+    var redirectBrowser = function () {
+        var uri = "http://www.example.com/";
+        window.location = uri;
+    };
+ 
+    var defaultSite = "world";
+ 
+    var onSuccess = function (geoipResponse) {
+        /* There's no guarantee that a successful response object
+         * has any particular property, so we need to code defensively. */
+        if (!geoipResponse.country.iso_code) {
+            console.log('no country code')
+            return;
+        }
+ 
+        /* ISO country codes are in upper case. */
+        var code = geoipResponse.country.iso_code.toLowerCase();
+ 
+        if ('gb' !== code) {
+            redirectBrowser();
+        }
+        
+    };
+ 
+    /* We don't really care what the error is, we'll send them
+     * to the default site. */
+    var onError = function (error) {
+        console.log('Error occured ...')
+    };
+ 
+    return function () {
+        geoip2.country( onSuccess, onError );
+    };
+}());
+ 
+redirect();
